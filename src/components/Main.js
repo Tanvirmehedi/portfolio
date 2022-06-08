b7886d8c0de8a39131796d7755804ce214cfe05d
Tanvirmehedi <tjmehedi201@gmail.com>
2022-06-09 04:44:46 +0600

    image section adfded

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ImageView from "./Main/ImageView";
 
 const Main = () => {
   return (
-    <div className="lg:w-1/2 px-3 text-justify bg-slate-100">
-      <h1 className="animate-bounce">hello</h1>
+    <div className="lg:w-1/2 text-justify bg-slate-100">
+      <ImageView />
     </div>
   );
 };
---
+++ src/components/Main/ImageView.js
@@ -0,0 +1,52 @@
+import React from "react";
+import api from "../../Assets/api.svg";
+import mern from "../../Assets/mern.svg";
+import react from "../../Assets/react.svg";
+import bg from "../../Assets/undraw_pair_programming_re_or4x.svg";
+
+const ImageView = () => {
+  const services = [
+    {
+      id: "1",
+      name: "Api",
+      img: api,
+    },
+    {
+      id: "2",
+      name: "React",
+      img: react,
+    },
+    {
+      id: "3",
+      name: "Mern",
+      img: mern,
+    },
+  ];
+  return (
+    <div className={`w-full h-screen bg-cyan-200`}>
+      <div>
+        <img src={bg} className="w-2/3 mx-auto py-10" alt="" />
+      </div>
+
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-4 mx-4">
+        {services.map((data) => {
+          return (
+            <div
+              key={data.id}
+              class="block p-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center uppercase flex justify-center items-center">
+                {data?.name}
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400 flex justify-center">
+                <img src={data?.img} className="mx-auto" alt="" />
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ImageView;
