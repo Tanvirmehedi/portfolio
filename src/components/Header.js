38e34312835644210b88b0c7016cd5f36fcbf9ce
Tanvirmehedi <tjmehedi201@gmail.com>
2022-06-09 02:13:33 +0600

    Header Section Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
             target="_blank"
             rel="noreferrer"
           >
-            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-cyan-100 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-cyan-400 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
               <span className="relative px-16 py-2.5 transition-all ease-in duration-75 bg-slate-600 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 ">
                 Resume
               </span>
